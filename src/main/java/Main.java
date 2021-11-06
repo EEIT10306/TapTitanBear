79c13e6312bdbfc4d738c1f5a6ba7ce10f2f1974
ZoroBear <jhhm8dp@gmail.com>
2021-11-06 23:14:56 +0800

    add sys Properties

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,32 @@ import command.Respond;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
-import javax.security.auth.login.LoginException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        JDA jda = JDABuilder.createDefault("OTA2NDE2MjIyMTUxNzk4ODE1.YYYT4w.9kCJfnW74f--uDYwCP8u1c9ZZdI").build();
+        initProperties();
+
+        JDA jda = JDABuilder.createDefault(System.getProperty("discord.token")).build();
 
         jda.addEventListener(new Respond());
         jda.awaitReady();
 
     }
+
+    public static void initProperties() throws IOException {
+        FileInputStream propFile = new FileInputStream( "config.properties");
+        Properties p = new Properties(System.getProperties());
+        p.load(propFile);
+
+        // set the system properties
+        System.setProperties(p);
+        // display new properties
+        System.getProperties().list(System.out);
+    }
+
 }
